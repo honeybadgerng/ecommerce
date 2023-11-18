8b9f92e4805586f69619b115c070a2a76e964af7
Honeybadger <rajimoshood200482@gmail.com>
2023-11-18 12:14:16 +0100

    fixed controller and route to recive orders

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,44 @@
-'use strict';
+"use strict";
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async create(ctx) {
+    const { billingAddress, userName, email, phoneNumber, products } =
+      ctx.request.body;
+
+    try {
+      const lineItems = await Promise.all(
+        products.map(async (product) => {
+          const item = await strapi
+            .service("api::item.item")
+            .findOne(product.id);
+
+          return {
+            price_data: {
+              currency: "ngn",
+              product_data: {
+                name: item.name,
+              },
+              unit_amount: item.price * 100,
+            },
+            quantity: product.count,
+          };
+        })
+      );
+
+      await strapi
+        .service("api::order.order")
+        .create({ data: { userName, products } });
+
+      return { message: "saved successfully" };
+    } catch (e) {
+      ctx.response.status = 500;
+      return { error: { message: "An Error occured " } };
+    }
+  },
+}));
--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -1,9 +1,9 @@
-'use strict';
-
-/**
- * order router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::order.order');
+module.exports = {
+  routes: [
+    {
+      method: "POST",
+      path: "/order",
+      handler: "order.create",
+    },
+  ],
+};
